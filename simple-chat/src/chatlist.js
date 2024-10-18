ce154dc75b72a1432bfd4b082ad07b89d9a7e4ad
BrashMack <brashmack1@gmail.com>
2024-10-18 03:51:58 +0300

    burger menu upgrade, visual upgrades, some errors fixed --v5

--- simple-chat/src/chatlist.js
+++ simple-chat/src/chatlist.js
@@ -3,6 +3,7 @@ import './chats.js';
 
 const burger = document.querySelector('.burger');
 const lines = document.querySelectorAll('.burger-line');
+const popupMenu = document.querySelector('.popup-menu');
 const chatList = document.querySelector('.chat-list');
 
 document.addEventListener('DOMContentLoaded', function loadChats() {
@@ -16,73 +17,91 @@ document.addEventListener('DOMContentLoaded', function loadChats() {
     chats.forEach((chat) => {
     addChat(chat.user, chat.avatar, chat.message, chat.time, chat.status.split(' '));
     });
-});   
+   
 
-function addChat(user, avatar, message, time, status) {
-    const chatElement = document.createElement('div');
-    chatElement.classList.add('chat-item');
-    chatElement.setAttribute('onclick', "window.location.href='index.html'");
+    function addChat(user, avatar, message, time, status) {
+        const chatElement = document.createElement('div');
+        chatElement.classList.add('chat-item');
+        if (user === "Дженнифер") {
+            chatElement.setAttribute('onclick', "window.location.href='index.html'");
+        }
+        else {
+            chatElement.setAttribute('onclick', 'alert("Этот чат пока не готов(")');
+        }
 
-    const imageSrc = document.createElement('img');
-    imageSrc.classList.add('avatar');
-    imageSrc.setAttribute('src', avatar);
-    imageSrc.setAttribute('alt', 'Chat_icon');
-    chatElement.appendChild(imageSrc);
+        const imageSrc = document.createElement('img');
+        imageSrc.classList.add('avatar');
+        imageSrc.setAttribute('src', avatar);
+        imageSrc.setAttribute('alt', 'Chat_icon');
+        chatElement.appendChild(imageSrc);
 
-    const userBlock = document.createElement('div');
-    userBlock.classList.add('user-block');
-    const chatName = document.createElement('h3');
-    chatName.classList.add('chat-name');
-    chatName.textContent = user;
-    const lastMessage = document.createElement('p');
-    lastMessage.classList.add('last-msg');
-    lastMessage.textContent = message;
-    userBlock.appendChild(chatName);
-    userBlock.appendChild(lastMessage);
-    chatElement.appendChild(userBlock);
+        const userBlock = document.createElement('div');
+        userBlock.classList.add('user-block');
+        const chatName = document.createElement('h3');
+        chatName.classList.add('chat-name');
+        chatName.textContent = user;
+        const lastMessage = document.createElement('p');
+        lastMessage.classList.add('last-msg');
+        lastMessage.textContent = message;
+        userBlock.appendChild(chatName);
+        userBlock.appendChild(lastMessage);
+        chatElement.appendChild(userBlock);
 
-    const timeBlock = document.createElement('div');
-    timeBlock.classList.add('time-block');
-    const lastTime = document.createElement('span');
-    lastTime.classList.add('time');
-    lastTime.textContent = time;
-    timeBlock.appendChild(lastTime);
-    let lastStatus;
-    switch (status[0]) {
-        case 'read':
-            lastStatus = document.createElement('i');
-            lastStatus.classList.add('material-symbols-outlined', 'checked');
-            lastStatus.textContent = 'done_all';
-            break;
-        case 'sent':
-            lastStatus = document.createElement('i');
-            lastStatus.classList.add('material-symbols-outlined', 'checked');
-            lastStatus.textContent = 'check';
-            break;
-        case 'unread':
-            lastStatus = document.createElement('span');
-            lastStatus.classList.add(status[0]);
-            lastStatus.textContent = status[1]
-            timeBlock.appendChild(lastStatus);
-            break;
+        const timeBlock = document.createElement('div');
+        timeBlock.classList.add('time-block');
+        const lastTime = document.createElement('span');
+        lastTime.classList.add('time');
+        lastTime.textContent = time;
+        timeBlock.appendChild(lastTime);
+        let lastStatus;
+        switch (status[0]) {
+            case 'read':
+                lastStatus = document.createElement('i');
+                lastStatus.classList.add('material-symbols-outlined', 'checked');
+                lastStatus.textContent = 'done_all';
+                break;
+            case 'sent':
+                lastStatus = document.createElement('i');
+                lastStatus.classList.add('material-symbols-outlined', 'checked');
+                lastStatus.textContent = 'check';
+                break;
+            case 'unread':
+                lastStatus = document.createElement('span');
+                lastStatus.classList.add(status[0]);
+                lastStatus.textContent = status[1]
+                timeBlock.appendChild(lastStatus);
+                break;
+        }
+        timeBlock.appendChild(lastStatus);
+        chatElement.appendChild(timeBlock);
+
+        chatList?.appendChild(chatElement);
+        if (chatList) {
+            chatList.scrollTop = chatList.scrollHeight;
+        }
     }
-    timeBlock.appendChild(lastStatus);
-    chatElement.appendChild(timeBlock);
 
-    chatList.appendChild(chatElement);
-    chatList.scrollTop = chatList.scrollHeight;
-}
+    burger?.addEventListener('click', function toggleBurger() {
+        lines.forEach((line) => line.classList.toggle('active'));
+        burger.classList.contains('active') ? popupMenu.style.display = 'none' : popupMenu.style.display = 'block';
+        burger.classList.toggle('active');
+    });
 
-burger.addEventListener('click', function toggleBurger() {
-    lines.forEach((line) => line.classList.toggle('active'));
-});
+    document.addEventListener('click', (event) => {
+        if (!event.target.closest('.burger') && burger?.classList.contains('active')) {
+            popupMenu.style.display = 'none';
+            burger.classList.toggle('active');
+            lines.forEach((line) => line.classList.toggle('active'));
+        }
+    });
 
-const createChatButton = document.querySelector('.create-chat');
+    const createChatButton = document.querySelector('.create-chat');
 
-createChatButton.addEventListener('mouseenter', () => {
-  createChatButton.style.transform = 'translateY(-5px)';
-});
+    createChatButton?.addEventListener('mouseenter', () => {
+    createChatButton.style.transform = 'translateY(-5px)';
+    });
 
-createChatButton.addEventListener('mouseleave', () => {
-  createChatButton.style.transform = 'translateY(0)';
+    createChatButton?.addEventListener('mouseleave', () => {
+    createChatButton.style.transform = 'translateY(0)';
+    });
 });
--- simple-chat/src/index.js
+++ simple-chat/src/index.js
@@ -8,6 +8,10 @@ const sendButton = document.querySelector('.submit');
 let chatId = 1;
 let messages = [];
 
+function hhMM() {
+    return new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+}
+
 document.addEventListener('DOMContentLoaded', function loadMessages() {
     for (let i = 1; i <= localStorage.length; ++i) {
         let message = JSON.parse(localStorage.getItem(`message${i}`));
@@ -18,84 +22,82 @@ document.addEventListener('DOMContentLoaded', function loadMessages() {
     messages.forEach((message) => {
     addMessage(message.text, message.time, message.isUser, message.img);
     });
-});
 
-function hhMM() {
-    return new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-}
+    function addMessage(message, time, isUser, img) {
+        const messageElement = document.createElement('div');
+        messageElement.classList.add('chat-message');
+        messageElement.classList.add(isUser ? 'user' : 'other');
 
-function addMessage(message, time, isUser, img) {
-    const messageElement = document.createElement('div');
-    messageElement.classList.add('chat-message');
-    messageElement.classList.add(isUser ? 'user' : 'other');
-
-    const messageContent = document.createElement('div');
-    if (!img) {
-        messageContent.classList.add('message-content');
-        messageContent.textContent = message;
-    }
-    else {
-        messageContent.classList.add('message-img');
-        const imageSrc = document.createElement('img');
-        imageSrc.classList.add('img');
-        imageSrc.setAttribute('src', img);
-        imageSrc.setAttribute('alt', 'Отправленное изображение');
-        messageContent.appendChild(imageSrc);
-    }
-
-    const messageTime = document.createElement('div');
-    const timeSpan = document.createElement('span');
-    messageTime.classList.add('message-time');
-    timeSpan.textContent = time;
-    if (isUser) {
-        const checkIcon = document.createElement('i');
-        checkIcon.classList.add('material-symbols-outlined', 'checked');
-        if (time < hhMM()) {
-            checkIcon.textContent = 'done_all';
+        const messageContent = document.createElement('div');
+        if (!img) {
+            messageContent.classList.add('message-content');
+            messageContent.textContent = message;
         }
         else {
-            checkIcon.textContent = 'check';
+            messageContent.classList.add('message-img');
+            const imageSrc = document.createElement('img');
+            imageSrc.classList.add('img');
+            imageSrc.setAttribute('src', img);
+            imageSrc.setAttribute('alt', 'Отправленное изображение');
+            messageContent.appendChild(imageSrc);
         }
-        messageTime.appendChild(checkIcon);
+
+        const messageTime = document.createElement('div');
+        const timeSpan = document.createElement('span');
+        messageTime.classList.add('message-time');
+        timeSpan.textContent = time;
+        if (isUser) {
+            const checkIcon = document.createElement('i');
+            checkIcon.classList.add('material-symbols-outlined', 'checked');
+            if (time < hhMM()) {
+                checkIcon.textContent = 'done_all';
+            }
+            else {
+                checkIcon.textContent = 'check';
+            }
+            messageTime.appendChild(checkIcon);
+        }
+        messageTime.appendChild(timeSpan);
+
+        messageContent.appendChild(messageTime);
+        messageElement.appendChild(messageContent);
+        chatBody?.appendChild(messageElement);
+        if (chatBody) {
+            chatBody.scrollTop = chatBody.scrollHeight;
+        }
     }
-    messageTime.appendChild(timeSpan);
 
-    messageContent.appendChild(messageTime);
-    messageElement.appendChild(messageContent);
-    chatBody.appendChild(messageElement);
-    chatBody.scrollTop = chatBody.scrollHeight;
-}
+    form?.addEventListener('submit', handleSubmit);
 
-form.addEventListener('submit', handleSubmit);
+    function handleSubmit(event) {
+        event.preventDefault();
+        const messageText = input.value.trim();
 
-function handleSubmit(event) {
-    event.preventDefault();
-    const messageText = input.value.trim();
+        if (messageText) {
+            messages.push({ text: messageText, time: hhMM(), isUser: true, img: "" });
+            localStorage.setItem(`message${localStorage.length + 1}`, JSON.stringify(messages[messages.length - 1]));
+            input.value = '';
+            addMessage(messageText, hhMM(), true, "");
 
-    if (messageText) {
-        messages.push({ text: messageText, time: hhMM(), isUser: true, img: "" });
-        localStorage.setItem(`message${localStorage.length + 1}`, JSON.stringify(messages[messages.length - 1]));
-        input.value = '';
-        addMessage(messageText, hhMM(), true, "");
+            updateLastMessage(messageText, hhMM());
+        }
+    };
 
-        updateLastMessage(messageText, hhMM());
-    }
-};
+    input?.addEventListener('keyup', (event) => {
+        if (event.key === 'Enter') {
+            handleSubmit(event);
+        }
+    });
 
-input.addEventListener('keyup', (event) => {
-    if (event.key === 'Enter') {
+    sendButton?.addEventListener('mouseup', (event) => {
         handleSubmit(event);
+    });
+
+    function updateLastMessage(text, time) {
+        const chat = JSON.parse(localStorage.getItem(`chat${chatId}`));
+        chat.message = text;
+        chat.time = time;
+        chat.status = 'sent';
+        localStorage.setItem(`chat${chatId}`, JSON.stringify(chat));
     }
 });
-
-sendButton.addEventListener('mouseup', (event) => {
-    handleSubmit(event);
-});
-
-function updateLastMessage(text, time) {
-    const chat = JSON.parse(localStorage.getItem(`chat${chatId}`));
-    chat.message = text;
-    chat.time = time;
-    chat.status = 'sent';
-    localStorage.setItem(`chat${chatId}`, JSON.stringify(chat));
-}
